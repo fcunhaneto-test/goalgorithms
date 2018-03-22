28f0404a7c428a808ab708391e12a7048ce14ba5
Francisco Cunha Neto <fcunhaneto@gmail.com>
2018-03-22 16:42:40 -0300

    change printDFS

--- graphs/dfs/printDFS.go
+++ graphs/dfs/printDFS.go
@@ -6,31 +6,20 @@ import (
 	"goalgorithms/linklist"
 )
 
-func printDFS(tail linklist.LL) {
-	n := tail.N.(*graphs.Vertex)
-	fmt.Println("*********************************")
-	fmt.Println("Vertex:", n.V)
-	fmt.Println("CC:", n.CC)
-	if n.P != nil {
-		fmt.Println("Predecessor:", n.P.V)
-	} else {
-		fmt.Println("Predecessor: null")
-	}
-	fmt.Printf("Time: %d/%d\n", n.TI, n.TF)
-	fmt.Println("*********************************")
-	for tail.Next != nil {
-		tail = tail.Next
-		if tail.Next != nil {
-			n = tail.N.(*graphs.Vertex)
-			fmt.Println("Vertex:", n.V)
-			fmt.Println("CC:", n.CC)
-			if n.P != nil {
-				fmt.Println("Predecessor:", n.P.V)
-			} else {
-				fmt.Println("Predecessor: null")
-			}
-			fmt.Printf("Time: %d/%d\n", n.TI, n.TF)
-			fmt.Println("*********************************")
+func printDFS(head linklist.LL) {
+	var n *graphs.Vertex
+	for head.Next != nil {
+		n = head.N.(*graphs.Vertex)
+		fmt.Println("Vertex:", n.V)
+		fmt.Println("CC:", n.CC)
+		if n.P != nil {
+			fmt.Println("Predecessor:", n.P.V)
+		} else {
+			fmt.Println("Predecessor: null")
 		}
+		fmt.Printf("Time: %d/%d\n", n.TI, n.TF)
+		fmt.Println("*********************************")
+
+		head = head.Next
 	}
 }
